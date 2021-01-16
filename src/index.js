0f09a0a97b00329b569068177a27d30be3e06e73
malkeeszon <rwitczak89@gmail.com>
2021-01-16 17:32:23 +0100

    feat(global): create client server

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-console */
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const SERVER_PORT = process.env.SERVER_PORT || 3001;
+const router = require('./router');
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+app.listen(SERVER_PORT, (err) => {
+  if (err) {
+    console.log(`😞 Sorry, something went wrong! ${err}`);
+  } else {
+    console.log(`🚀 Server is listening on port ${SERVER_PORT}!`);
+  }
+});
