6bd6f674f8fe15202b4a4def57144ea3c0d4a96a
malkeeszon <rwitczak89@gmail.com>
2021-01-17 16:04:39 +0100

    feat(router): add avaialbe all routes

--- src/router.js
+++ src/router.js
@@ -1,5 +1,24 @@
 const router = require('express').Router();
+const { getAllClients } = require('./controllers/clients.controller');
 
+router.post('/login', getAllClients);
+
+router.get('/policies', (req, res) => {
+  res.send("you hit the endpoint 'policies'");
+});
+router.get('/policies/:id', (req, res) => {
+  res.send("you hit the endpoint 'policies/:id'");
+});
+
+router.get('/clients', (req, res) => {
+  res.send("you hit the endpoint 'clients'");
+});
+router.get('/clients/:id', (req, res) => {
+  res.send("you hit the endpoint 'clients/:id'");
+});
+router.get('/clients/:id/policies', (req, res) => {
+  res.send("you hit the endpoint 'clients:id/policies'");
+});
 router.get('*', (req, res) => {
   res.status(404).send('Sorry, not found 😞');
 });
