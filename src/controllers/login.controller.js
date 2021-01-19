413d7d08b363868f71f89e6c22b8119457569b2f
malkeeszon <rwitczak89@gmail.com>
2021-01-19 04:12:05 +0100

    fix(authentication): fix user authentication to respond with unique tokens for each user

--- src/controllers/login.controller.js
+++ src/controllers/login.controller.js
@@ -1,19 +1,45 @@
-const axios = require('axios');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const { handleError } = require('../helpers/functions');
 
-const { CLIENT_ID, CLIENT_SECRET, AUTHENTICATION_ENDPOINT } = process.env;
+const SECRET_KEY = process.env.SECRET_KEY || 'lalala this isnt secure';
+const {
+  renewInsuranceToken,
+  fetchAllClients,
+} = require('../helpers/apiService');
 
-async function loginUser(_req, res) {
+const {
+  handleError,
+  checkIfClientExistAndVerifyRole,
+} = require('../helpers/functions');
+
+const { myCache } = require('../helpers/cache');
+
+async function authenticateUser(req, res) {
+  const { username } = req.body;
+  let insuranceToken = myCache.get('insurance_token');
+  if (insuranceToken === undefined) {
+    await renewInsuranceToken();
+    insuranceToken = myCache.get('insurance_token');
+  }
   try {
-    const result = await axios.post(AUTHENTICATION_ENDPOINT, {
-      client_id: CLIENT_ID,
-      client_secret: CLIENT_SECRET,
-    });
-    res.status(200).send(result.data);
+    const clients = await fetchAllClients(
+      insuranceToken.type,
+      insuranceToken.token
+    );
+    const user = checkIfClientExistAndVerifyRole(clients.data, username);
+
+    if (!user) {
+      res.status(400).send({ code: 400, message: 'User not found' });
+    } else {
+      const userToken = jwt.sign({ username }, SECRET_KEY);
+      myCache.set(username, `${user.role} ${user.id}`);
+      res
+        .status(200)
+        .send({ token: userToken, type: 'Bearer', expires_in: 560 });
+    }
   } catch (error) {
     handleError(res, error);
   }
 }
 
-module.exports = { loginUser };
+module.exports = { authenticateUser };
