2a690f67f3b6834b127c6704e61c907d58c1cb7e
Rafal Witczak <rwitczak89@gmail.com>
2021-02-02 12:02:45 +0100

    fix(authorizeuser): add http-erros to handle 401 Unauthorized error

--- src/middleware/authorization.js
+++ src/middleware/authorization.js
@@ -1,9 +1,10 @@
 const jwt = require('jsonwebtoken');
+const createError = require('http-errors');
 const { myCache } = require('../helpers/cache');
 
 const SECRET_KEY = process.env.SECRET_KEY || 'lalala this isnt secure';
 
-function authorizeUser(req, _, next) {
+function authorizeUser(req, res, next) {
   const token = req.headers.authorization.split(' ')[1];
 
   try {
@@ -11,7 +12,12 @@ function authorizeUser(req, _, next) {
     req.user = myCache.get(decoded.username).split(' ');
     next();
   } catch (error) {
-    throw new Error('Invalid token! Please login.');
+    next(
+      createError(401, {
+        code: 401,
+        message: `Invalid token, please login.`,
+      })
+    );
   }
 }
 
