a71042a620829b7a1a895f2152e2394455e684cc
Rafal Witczak <rwitczak89@gmail.com>
2021-02-11 21:38:22 +0100

    refactor(login.controller): refactor tests to use asyn/await

--- src/__tests__/login.controller.js
+++ src/__tests__/login.controller.js
@@ -1,32 +1,28 @@
 const request = require('supertest');
-const app = require('../index');
+const app = require('..');
 
 describe('POST /login', () => {
-  test('LOGIN - success', () => {
+  test('LOGIN - success', async () => {
     expect.assertions(4);
-    return request(app)
+    const response = await request(app)
       .post('/login')
       .send({ username: 'britneyblankenship@quotezart.com' })
-      .expect(200)
-      .then((response) => {
-        expect(response.body.token).toBeTruthy();
-        expect(response.body.token.length).toBe(189);
-        expect(response.body.type).toBeTruthy();
-        expect(response.body.type).toBe('Bearer');
-      });
+      .expect(200);
+    expect(response.body.token).toBeTruthy();
+    expect(response.body.token.length).toBe(189);
+    expect(response.body.type).toBeTruthy();
+    expect(response.body.type).toBe('Bearer');
   });
 
-  test('LOGIN - failure', () => {
+  test('LOGIN - failure', async () => {
     expect.assertions(4);
-    return request(app)
+    const response = await request(app)
       .post('/login')
       .send({ username: 'nonexistinguser@email.com' })
-      .expect(400)
-      .then((response) => {
-        expect(response.body.code).toBeTruthy();
-        expect(response.body.code).toBe(400);
-        expect(response.body.message).toBeTruthy();
-        expect(response.body.message).toBe('User not found');
-      });
+      .expect(400);
+    expect(response.body.code).toBeTruthy();
+    expect(response.body.code).toBe(400);
+    expect(response.body.message).toBeTruthy();
+    expect(response.body.message).toBe('User not found');
   });
 });
